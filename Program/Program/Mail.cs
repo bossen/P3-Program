721ac8506aa43865022863832161c3fc92720416
ThaDane <nic.lukasso@gmail.com>
2013-11-13 15:48:56 +0100

    Added Mail class
    Not yet ready for implementation.
    Only made the skeletal structure

---
+++ Program/Program/Mail.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Mail
+    {
+        #region Properties
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Methods
+        private void SendMail(/*object sender, EventArgs e*/)
+        {
+            throw new NotImplementedException();
+
+            /*// inspiration fra: http://tutplusplus.blogspot.dk/2010/09/c-tutorial-mail-sender-send-e-mails.html
+            try
+            {
+                string senderMail = "kostplantjekliste@gmail.com";
+                string recieverMail = txtMailTjekRecieverMail.Text;
+                
+                MailMessage mail = new MailMessage(
+                    senderMail,    //sender
+                    recieverMail,  //modtager
+                    CurrentUser.Name + (CurrentUser.Name.EndsWith("s") ? "' tjekliste" : "s tjekliste"),   //emne (Navn's tjekliste)
+                    txtTjeklisteOutput.Text); //tekst
+
+                SmtpClient client = new SmtpClient("smtp.gmail.com");
+                client.Port = 587;
+                client.Credentials = new System.Net.NetworkCredential(senderMail, "SW2A219AAU");
+                client.EnableSsl = true;
+                client.Send(mail);
+                MessageBox.Show("Mailen blev sendt.", "Succes!", MessageBoxButtons.OK);
+                txtMailTjekRecieverMail.Text = "";
+            }
+            catch (System.Net.Mail.SmtpException)
+            {
+                MessageBox.Show("Ugyldige informationer. Prøv igen.");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Der skete en fejl; mailen blev ikke sendt.");
+            }*/
+        }
+#endregion
+    }
+}
