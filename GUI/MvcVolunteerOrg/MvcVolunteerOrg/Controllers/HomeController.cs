b2c3b1559d0f274bf3114dec070731db594bf478
Jannek Bossen <jannek.boss@gmail.com>
2013-12-06 00:39:15 -0800

    added dummy objects creator to database

--- GUI/MvcVolunteerOrg/MvcVolunteerOrg/Controllers/HomeController.cs
+++ GUI/MvcVolunteerOrg/MvcVolunteerOrg/Controllers/HomeController.cs
@@ -16,12 +16,27 @@ namespace MvcVolunteerOrg.Controllers
     {
         public ActionResult Index()
         {
+            //Adding dummy objects to database
+            using (var db = new VolunteerOrgContext()
+            {
+                db.Database.ExecuteSqlCommand("delete from VolunteerProjects");
+                
+                Organization o1 = new Organization("Green", new Location("Lundvej 2", "Aalborg"), "contact@green.com");
+                Organization o2 = new Organization("Blue", new Location("Strandsevej 53", "Aalborg"), "contact@blue.com");
+                Organization o3 = new Organization("Red", new Location("Råbærvej 101", "Aalborg"), "contact@red.com");
+            
+                db.Organizations.Add(o1);
+                db.Organizations.Add(o2);
+                db.Organizations.Add(o3);
+
+                db.SaveChanges();
+            }
+
+
             string name = WebSecurity.IsAuthenticated ? WebSecurity.CurrentUserName : "guest";
             ViewBag.Title = "Welcome " + name + ".";
             using (var db = new VolunteerOrgContext())
             {
-                db.Database.ExecuteSqlCommand("delete from VolunteerProjects");
-                db.SaveChanges();
                 ViewBag.VolunteerProjects = db.VolunteerProjects.ToList().OrderBy(b => b.Time);
             }
 
