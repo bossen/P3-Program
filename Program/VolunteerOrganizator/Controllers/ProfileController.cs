e52b354d3d658b1af54c5c778d782fcfd3571d96
netray92 <netray56@hotmail.com>
2013-12-08 17:21:17 +0100

    Added new Project for simple login
    Doesn't work fully yet.

---
+++ Program/VolunteerOrganizator/Controllers/ProfileController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+
+namespace MvcVolunteerOrg.Controllers
+{
+    public class ProfileController : Controller
+    {
+        //
+        // GET: /Profile/
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Profile/Edit/5
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Volunteer volunteer)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Profile");
+            }
+
+            return View(volunteer);
+        }
+
+        public ActionResult CreateOrNot()
+        {
+            
+            return View();
+        }
+        
+    }
+}
---
+++ Program/VolunteerOrganizator/Models/VolunteerOrganizatorContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Model;
+using System.Data.Entity;
+
+namespace VolunteerOrganizator.Models
+{
+    public class VolunteerOrganizatorContext : DbContext
+    {
+        // Initialize Database from different classes.
+        public DbSet<Volunteer> Volunteers { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<VolunteerProject> VolunteerProjects { get; set; }
+        public DbSet<Match> Matches { get; set; }
+    }
+}
