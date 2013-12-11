da2782d5b33a6ad5c5c85718ca8fe532d7406f05
Jannek Bossen <jbossen12@student.aau.dk>
2013-12-11 12:14:18 +0100

    Suggestion home fix

--- Program/TestWeb2/Controllers/HomeController.cs
+++ Program/TestWeb2/Controllers/HomeController.cs
@@ -22,11 +22,11 @@ namespace TestWeb2.Controllers
             Organization o1 = new Organization("green", new Location("address", "city"), "mail@mail.dk");
             VolunteerProject vp1 = o1.CreateProject("title", new Location("address", "city"), DateTime.Now.AddDays(2), new List<Preference>() { Preference.Church }, "description");
             Suggestion s1 = v1.AddSuggestion(vp1);
-
-            //Volunteer ole = db.Volunteers.ToList().Where(v => v.UserName == "ole").FirstOrDefault();
-            //ole.AddSuggestion(vp1);
-            //Invite i1 = new Invite(ole, vp1);
-            //ole.AddMatch(i1);
+            db.VolunteerProjects.Add(vp1);
+            Volunteer ole = db.Volunteers.ToList().Where(v => v.UserName == "ole1").FirstOrDefault();
+            ole.AddSuggestion(vp1);
+            Invite i1 = new Invite(ole, vp1);
+            ole.AddMatch(i1);
 
             db.Organizations.Add(o1);
             db.Volunteers.Add(v1);
@@ -41,7 +41,13 @@ namespace TestWeb2.Controllers
 
                 if (currentUser.GetInvites() != null)
                     ViewBag.Invites = currentUser.GetInvites();
-                ViewBag.Suggestions = currentUser.GetSortMatches();
+
+                List<VolunteerProject> projectSuggestions = new List<VolunteerProject>();
+                foreach (Match match in currentUser.GetSortMatches())
+	            {
+                    projectSuggestions.Add(match.Project);
+	            }
+                ViewBag.Suggestions = projectSuggestions;
             }
             else
             {
@@ -59,8 +65,10 @@ namespace TestWeb2.Controllers
             return View();
         }
 
-        public ViewResult Project(VolunteerProject project)
+        public ViewResult Project(string projectName)
         {
+            VolunteerProject project = db.VolunteerProjects.Where(p => p.Title == projectName).FirstOrDefault();
+
             return View(project);
         }
 
