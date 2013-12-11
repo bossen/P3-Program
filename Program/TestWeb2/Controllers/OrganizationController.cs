1084e3c152e9fc72902c6143b0fd060fa3047c15
netray92 <netray56@hotmail.com>
2013-12-11 14:19:08 +0100

    Updated Views and controllers

---
+++ Program/TestWeb2/Controllers/OrganizationController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestWeb2.Controllers
+{
+    public class OrganizationController : Controller
+    {
+        //
+        // GET: /Organization/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateOrNot()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult AllProjects()
+        {
+            return View();
+        }
+
+        public ActionResult CreateProject()
+        {
+            return View();
+        }
+
+        public ActionResult EditProject()
+        {
+            return View();
+        }
+
+    }
+}
