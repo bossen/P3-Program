ae2f9fef8d9cec950fb8674fd96f6aef97973716
netray92 <netray56@hotmail.com>
2013-12-12 14:25:49 +0100

    Hotfix

--- Program/TestWeb2/Models/MultiViewModel.cs
+++ Program/TestWeb2/Models/MultiViewModel.cs
@@ -8,15 +8,9 @@ namespace TestWeb2.Models
 {
     public class MultiViewModel
     {
-<<<<<<< HEAD
-        public Admin Admin { get; set; }
-        public Organization Organization { get; set; }
-        public Volunteer Volunteer { get; set; }
-=======
         public Admin Admin { get; private set; }
         public Organization Organization { get; private set; }
         public Volunteer Volunteer { get; private set; }
         public VolunteerProject VolunteerProject { get; set; }
->>>>>>> a0b0f2ddbe400334450b59e682cb7eb14179c7fa
     }
 }
