a8affaa0c5ea23594fe82e866b03a01b4db3226c
netray92 <netray56@hotmail.com>
2013-12-12 11:29:42 +0100

    Added a little Change to Controllers and views

--- Program/TestWeb2/Controllers/AdminController.cs
+++ Program/TestWeb2/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,50 @@ namespace TestWeb2.Controllers
 {
     public class AdminController : Controller
     {
+        private VolunteerOrgContext db = new VolunteerOrgContext();
         //
         // GET: /Admin/
 
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Organization organization)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Organizations.Add(organization);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(organization);
+        }
+
+        public ActionResult Edit(int id = 0)
+        {
+            Organization organization = db.Organizations.Find(id);
+            if (organization == null)
+            {
+                return HttpNotFound();
+            }
+            return View(organization);
+        }
+
+        public ActionResult Organizations()
+        {
+            return View();
+        }
+
+        public ActionResult OrganizationDetails()
         {
             return View();
         }
